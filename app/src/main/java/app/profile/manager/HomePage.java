c8db25ff7eee3d0c4c7b844bfc07734a26e18e2d
Pak_Man926 <davidkamau925@gmail.com>
2025-01-28 02:47:22 +0300

    Second Commit

--- app/src/main/java/app/profile/manager/HomePage.java
+++ app/src/main/java/app/profile/manager/HomePage.java
@@ -1,6 +1,12 @@
 package app.profile.manager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity
@@ -11,4 +17,20 @@ public class HomePage extends AppCompatActivity
         super.onCreate(savedInstance);
         setContentView(R.layout.homepage);
     }
+
+    public void showPopup(View v)
+    {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.actions, popup.getMenu());
+        popup.show();
+    }
+
+    public void OnMenuItemClick(MenuItem item)
+    {
+        Intent intent = new Intent(HomePage.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
